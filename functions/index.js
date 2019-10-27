d36a4542a3f02cc2f4c4dfd98cc31765101a0a7e
unknown <josephgodwin274@yahoo.com>
2019-10-27 04:16:05 +0100

    Firebase functions

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const functions = require('firebase-functions');
+
+
+exports.GenerateUuid = functions.database.instance('feature-godwin-enye').ref('/users/{UserId}')
+    .onCreate((snapshot, context) => {
+        console.log(snapshot.val());
+        console.log(context);
+        const uuidv5 = require('uuid/v5');
+        const ENYE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+        const code = uuidv5('enye-id', ENYE);
+        return snapshot.ref.update({ userId : code});
+    });
